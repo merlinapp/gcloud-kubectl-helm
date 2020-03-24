8235661a1be67b584bc67d3de117f5810464385a
Juan Carlos <ruluk@users.noreply.github.com>
2020-03-24 14:21:57 -0500

    Feat: Send proxy ports as shell params.
    Signed-off-by: Juan Carlos <ruluk@users.noreply.github.com>

--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-haproxy -f /proxy/kube-port-forward
+if [ -n "$1" ] && [ -n "$2" ]; then
+  export PUBLISHED_PORT=$1
+  export KUBE_PORT=$2
+  haproxy -f /proxy/kube-port-forward
+else
+  echo "Please send two parameters: the exposed port and the kubectl port"
+fi
