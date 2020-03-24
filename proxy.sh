7af5d8b3b27e279637c4a397bc38ec3377f94dea
Juan Carlos <ruluk@users.noreply.github.com>
2020-03-24 15:42:55 -0500

    Feat: Report when port is being forwarded.
    Signed-off-by: Juan Carlos <ruluk@users.noreply.github.com>

--- proxy.sh
+++ proxy.sh
@@ -4,6 +4,7 @@ if [ -n "$1" ] && [ -n "$2" ]; then
   export PUBLISHED_PORT=$1
   export KUBE_PORT=$2
   haproxy -f /proxy/kube-port-forward
+  echo "Forwarding from port $PUBLISHED_PORT to $KUBE_PORT"
 else
   echo "Please send two parameters: the exposed port and the kubectl port"
 fi
